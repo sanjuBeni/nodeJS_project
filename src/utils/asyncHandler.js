20237e218f933c362162ff7316efaa70bb3f4739
sanjauBeni <sanjaysewa999@gmail.com>
2024-09-05 23:30:22 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,15 @@
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+};
+
+export { asyncHandler };
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.send(error.code || 500).json({
+//       success: true,
+//       message: error.message,
+//     });
+//   }
+// };
